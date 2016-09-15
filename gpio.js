9cb24151bbb15f92b36085105d632e5857a92601
A1kerr <A1kerr>
2016-09-15 17:21:21 -0400

    Added error handling and soft exit functions for GPIO using Linux unexport.

--- gpio.js
+++ gpio.js
@@ -5,6 +5,17 @@ var led6 = new Gpio(6,'out');
 var button = new Gpio(5,'in','both');
 
 button.watch(function(err, value){
-led.writeSync(value);
+    if(err){
+        throw err;
+    };
+led4.writeSync(value);
+led6.writeSync(value);
+});
+
+process.on('SIGINT', function(){
+    led4.unexport();
+    led6.unexport();
+    button.unexport();
+    console.log('GPIO Unexport Completed');
 });
 
