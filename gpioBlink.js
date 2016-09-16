6681a9331e2657d930e115621cb3d4e45e9231ca
A1kerr <A1kerr>
2016-09-15 23:11:49 -0400

    added timing argument

--- gpioBlink.js
+++ gpioBlink.js
@@ -4,29 +4,34 @@ var led4 = new Gpio(4,'out','none',true);
 var led6 = new Gpio(6,'out','none',false);
 
 
-function callBlink(){
+function callBlink(led, hz, timeB){
+    var timeB = timeB || 5000;
+    var hz = hz || 250; 
+    var ledRW = {};
+    ledRW = led;
+
 
 var ledBlink = setInterval(function(){
-    var value = led4.readSync();
+    var value = ledRW.readSync();
     value = Number(!value);
-    led4.writeSync(value);
-    console.log('Led Value: ' + value);
-    },250);
+    ledRW.writeSync(value);
+    console.log('Led Value: ' + value + ' ' + hz);
+    },hz);
 
 setTimeout(function(){
-    console.log('Blink Done!');
+    console.log('Blink Done!' + ' ' + timeB);
     clearInterval(ledBlink);
-},10000);
+},timeB);
 
-process.on('SIGINT', function(){
-    led4.unexport();
+process.on('SIGINT', function(led){
+    var led = led.unexport();
     //led6.unexport();
     //button.unexport();
     console.log('GPIO Unexport Completed');
 });
 }
 
-callBlink();
+callBlink(led4,250,10000);
 
 
 
